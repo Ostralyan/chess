090d0da411d331250ad202e239d59f067e52c633
Luke Xu <lukecxu@gmail.com>
2017-05-26 22:19:15 -0400

    Wrote the __str__ function for board to print out the board on demand

--- board/board.py
+++ board/board.py
@@ -1,8 +1,21 @@
+from .coordinate import Coordinate
+
+
 class Board():
     def __init__(self, white_player, black_player):
         self.board = {}
-        for piece in white_player.available_pieces:
-            self.board[piece.coordinate] = piece 
-        for piece in black_player.available_pieces:
-            self.board[piece.coordinate] = piece 
-        print(self.board, "\n")
+        for piece in white_player.available_pieces + \
+                black_player.available_pieces:
+            self.board[piece.coordinate] = piece
+
+    def __str__(self):
+        result = ""
+        for i, coord in enumerate(Coordinate):
+            piece = self.board.get(coord, None)
+            if i % 8 == 0:
+                result += "\n"
+            if piece is not None:
+                result += str(piece) + " "
+            else:
+                result += "_ "
+        return result
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from players.player import Human
 from players.color import Color
 from board.board import Board
+from board.coordinate import Coordinate
+
 
 def main():
     white = Human(Color.WHITE)
     black = Human(Color.BLACK)
+    board = Board(white, black)
 
-    Board(white, black)
+    print(board)
+
 
 if __name__ == "__main__":
     main()
-    
