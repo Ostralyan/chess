6d13d0431d2d762d8c952b7d02c933da55baa02b
ericZ <ericzhuang93@gmail.com>
2017-06-08 22:22:29 -0400

    pawn stuff almost done

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from players.color import Color
 from board.board import Board
 from board.coordinate import Coordinate
 
-from pieces.piece import King
+from pieces.piece import King, Pawn
 
 
 def main():
@@ -13,10 +13,15 @@ def main():
 
     print(board)
 
-    print(board.board[Coordinate.d8].is_on_board)
-    print(board.board[Coordinate.e8].is_on_board)
+    # king = King(Coordinate.e1, Color.WHITE)
+    # king.get_possible_moves()
+    # print(king.get_possible_moves)
 
+    pawn = Pawn(Coordinate.b2, Color.WHITE)
+    pawn.get_possible_moves()
+    print(pawn.get_possible_moves)
 
+    print(board.board[Coordinate.a2].is_on_board)
 
 
 if __name__ == "__main__":
--- pieces/piece.py
+++ pieces/piece.py
@@ -33,14 +33,16 @@ class King(Piece):
         raise NotImplementedError
 
     def get_possible_moves(self):
-        possible_moves = []
+        possible_moves = []  
         for x in range(-1, 2):
             for y in range(-1, 2):
                 possible_move = tuple(
                         map(sum, zip(self.coordinate.value, (x, y))))
                 if (self.is_on_board(possible_move)):
                     possible_moves.append(Coordinate(possible_move))
+        print("selfcoordvalue", self.coordinate.value)
         possible_moves.remove(self.coordinate)
+        print(possible_moves)
         return possible_moves
 
     def __str__(self):
@@ -110,13 +112,36 @@ class Pawn(Piece):
     def is_move_valid(self):
         return True
 
+    # Pawn unique starting move
+    def is_starting_position(starting_coordinate):
+        valid_start_coordinate = [(1, 4), (2, 4), (3, 4), (4, 4), (5, 4), (6, 4), (7, 4), (8, 4)]
+        if (starting_coordinate.value in valid_start_coordinate):
+            # pass in coords from coord.Coord or numerical values work? diff?
+            return True  # 16 checks for valid pawn starting positions
+
     def get_possible_moves(self):
-        raise NotImplementedError
+        possible_moves = []
+        starting_move = tuple(map(sum, zip(self.coordinate.value, (0, 2))))
+        print("starting move", starting_move)
+        # if pos is valid start pos, possible_moves.append starting_move
+        if (self.is_on_board(starting_move) and
+                self.is_starting_position(starting_move)):
+            possible_moves.append(Coordinate(starting_move))
+        # possible_moves.remove(self.coordinate)
+
+        for x in range(-1, 2):
+            for y in range(0, 2):
+                possible_move = tuple(
+                    map(sum, zip(self.coordinate.value, (x, y))))
+                if (self.is_on_board(possible_move)):
+                    possible_moves.append(Coordinate(possible_move))
+        print("selfcoordvaluepawn", self.coordinate.value)
+        possible_moves.remove(self.coordinate)
+        print(possible_moves)
+        return possible_moves
 
     def __str__(self):
         if self.color == Color.WHITE:
             return u'\u2659'
         elif self.color == Color.BLACK:
             return u'\u265F'
-
-
